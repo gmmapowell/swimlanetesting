c05d79c8786c04e071e546975321bea56f6cf107
Gareth Powell <gareth@ziniki.org>
2017-05-01 15:07:36 -0400

    Introduced TestResultReporter interface

---
+++ com.gmmapowell.swimlane/src/main/java/com/gmmapowell/swimlane/eclipse/interfaces/TestResultReporter.java
@@ -0,0 +1,7 @@
+package com.gmmapowell.swimlane.eclipse.interfaces;
+
+// Possibly not the best name, because this is the guy that *is* notified,
+// not the one doing the reporting
+public interface TestResultReporter {
+
+}
