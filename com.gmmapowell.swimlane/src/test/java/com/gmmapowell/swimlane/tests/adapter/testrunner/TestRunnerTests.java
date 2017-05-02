f57398f1041eb6d63c878dec1fd69baf008c2b5b
Gareth Powell <gareth@ziniki.org>
2017-05-02 07:39:37 -0400

    Removed dependency on physical HexagonAccumulator class

--- com.gmmapowell.swimlane/src/test/java/com/gmmapowell/swimlane/tests/adapter/testrunner/TestRunnerTests.java
+++ com.gmmapowell.swimlane/src/test/java/com/gmmapowell/swimlane/tests/adapter/testrunner/TestRunnerTests.java
@@ -1,36 +1,45 @@
 package com.gmmapowell.swimlane.tests.adapter.testrunner;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNotNull;
-
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import org.jmock.Expectations;
 import org.junit.Test;
 
+import com.gmmapowell.swimlane.eclipse.interfaces.Accumulator;
+import com.gmmapowell.swimlane.eclipse.interfaces.HexagonDataModel;
 import com.gmmapowell.swimlane.eclipse.interfaces.ModelDispatcher;
+import com.gmmapowell.swimlane.eclipse.interfaces.TestResultReporter;
 import com.gmmapowell.swimlane.eclipse.interfaces.TestRunner;
-import com.gmmapowell.swimlane.eclipse.models.HexagonAccumulator;
+import com.gmmapowell.swimlane.eclipse.models.TestGroup;
 import com.gmmapowell.swimlane.eclipse.views.RunAllTestsAction;
 import com.gmmapowell.swimlane.tests.swtutil.TestBase;
 
 public class TestRunnerTests extends TestBase {
 
+	interface HexAcc extends TestResultReporter, Accumulator, HexagonDataModel {
+	}
+	
 	@Test
 	public void testThatTheDateIsUpdatedWhenWePushTheRunButton() {
 		ModelDispatcher md = context.mock(ModelDispatcher.class);
 		TestRunner tr = context.mock(TestRunner.class);
 		RunAllTestsAction action = new RunAllTestsAction(tr, md);
-		HexagonAccumulator hex = new HexagonAccumulator();
+		HexAcc hex = context.mock(HexAcc.class);
 		action.setModel(hex);
-		Date nd = new Date();
+		String cp = "a:b:c";
+		List<TestGroup> tcs = new ArrayList<>();
+		TestGroup tg = new TestGroup(cp);
+		tcs.add(tg);
+		tg.addTest("com.foo");
 		context.checking(new Expectations() {{
-			oneOf(tr).runClass("", "");
+			oneOf(hex).getAllTestClasses(); will(returnValue(tcs));
+			oneOf(tr).runClass(hex, cp, "com.foo");
 			oneOf(md).setModel(hex);
+			oneOf(hex).testsCompleted(with(any(Date.class)));
 		}});
 		action.run();
-		assertNotNull("The test completion time was not set", hex.getTestCompleteTime());
-		assertFalse("The test completion time was not not after the current time", nd.after(hex.getTestCompleteTime()));
 	}
 
 }
